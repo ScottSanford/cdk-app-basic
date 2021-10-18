b4246facb1e92f66493fb173cb3c3b0e7d9ead4d
Scott Sanford <ssanford@mediafly.com>
2021-10-18 09:40:06 -0500

    basic app

--- lib/cdk-app-basic-stack.ts
+++ lib/cdk-app-basic-stack.ts
@@ -1,9 +1,23 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as ecs from '@aws-cdk/aws-ecs'
+import * as ec2 from '@aws-cdk/aws-ec2'
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns'
 
 export class CdkAppBasicStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
+    // Create VPC & Fargate Cluster
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 })
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc })
+    
+    // Instantiate Fargate Service with just cluster and image
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'FargateService', {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample')
+      }
+    })
   }
 }
